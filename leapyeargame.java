e5c04928058be401cc44a28a6c9ce7292ac75b41
Rugan254 <118251089+Rugan254@users.noreply.github.com>
2022-11-15 16:56:14 +0300

    Create leapyeargame.java

---
+++ leapyeargame.java
@@ -0,0 +1,30 @@
+/* 
+ * the user enters the year and the porgram checks and determine if it is a leapyear or not
+*/
+package Riggy;
+
+import java.util.Scanner;
+
+public class leapyeargame 
+{
+	
+	public static void main(String[]args)
+	{
+	int number_entered;
+	Scanner input =new Scanner(System.in);
+	System.out.print("hey kid,kindly enter the year to be tested");
+	number_entered=input.nextInt();
+//we use modulus because when we divide by 4 it should  be divisible by 4 for it to be a leap year
+	System.out.print("\n");
+if (number_entered  %4==0) 
+{
+	System.out.println(" we have entered into a leap year"+number_entered+"is not a leap year");//outputs leap year if true
+}
+
+
+else {
+	System.out.println( "we have not yet reached the leap year");//outputs have not yet reached if false
+}}
+
+
+}
