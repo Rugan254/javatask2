3ac4614ed486f68d1679a9a10dd039e593aa2840
Rugan254 <118251089+Rugan254@users.noreply.github.com>
2022-11-15 16:50:29 +0300

    Create (non)staticconstructor.java

---
+++ (non)staticconstructor.java
@@ -0,0 +1,47 @@
+/*
+Static constructor do not have any access specifier but non-static constructor have access specifier*/
+package Riggy;
+
+public class (non)staticconstructor {
+	public class Main {
+		static int manyasa(int boom,int been){// this is the static method
+		return boom*been;
+	}
+		
+			
+			public static void main (String[]args)
+			
+			{
+				
+			int riggy=nonstatic_static_constructor.product(159,290);//here is the non static constructor
+			
+			System.out.print(riggy);
+			}
+
+			private static int product(int val1, int val2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			}
+
+	public static int product(int val1, int val2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
